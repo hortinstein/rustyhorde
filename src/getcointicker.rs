4766b9c2805f7aba6db488f3d20f030d3bf52cc1
Alex Hortin <hortinstein@gmail.com>
2018-03-01 04:48:21 +0000

    got rid of a lot of the warnings

--- src/getcointicker.rs
+++ src/getcointicker.rs
@@ -1,7 +1,8 @@
 extern crate reqwest;
 
 use std::io::Read;
-use reqwest::get;
+//use reqwest::get;
+
 /// returns a list of coin prices from coinmarketcap
 ///
 /// # Arguments
@@ -20,7 +21,6 @@ use reqwest::get;
 /// println!("{}",cp);  
 /// ```
 pub fn coinprices(limit: i32) ->  Result<String, reqwest::Error> {
-    /// 
     let mut uri: String = "https://api.coinmarketcap.com/v1/ticker/?limit=".to_owned();
     uri.push_str(&(limit.to_string()));
   
@@ -28,7 +28,7 @@ pub fn coinprices(limit: i32) ->  Result<String, reqwest::Error> {
     assert!(resp.status().is_success());
 
     let mut content = String::new();
-    resp.read_to_string(&mut content);
+    resp.read_to_string(&mut content).expect("could not read prices");;
     Ok(content)
 }
 
@@ -50,7 +50,7 @@ pub fn coinprices(limit: i32) ->  Result<String, reqwest::Error> {
 /// println!("{}",cp);  
 /// ```
 pub fn coinprice_id(id: String) ->  Result<String, reqwest::Error> {
-    /// 
+     
     let mut uri: String = "https://api.coinmarketcap.com/v1/ticker/".to_owned();
     uri.push_str(&id);
   
@@ -58,7 +58,7 @@ pub fn coinprice_id(id: String) ->  Result<String, reqwest::Error> {
     assert!(resp.status().is_success());
 
     let mut content = String::new();
-    resp.read_to_string(&mut content);
+    resp.read_to_string(&mut content).expect("could not read prices");
     Ok(content)
 }
 
@@ -70,13 +70,19 @@ pub fn coinprice_id(id: String) ->  Result<String, reqwest::Error> {
 extern crate serde_json;
 // JSON Parsing and Construction
 // https://github.com/serde-rs/json
+
+
+//unused imports are allowed here because this is just for test code
+#[allow(unused_imports)]
 use serde_json::from_str;
 
+#[allow(unused_imports)]
+use serde_json::Value;
 #[test]
 fn test_pull_id() {
     let cp = match coinprice_id("bitcoin".to_owned()) {
     Result::Ok(val) => {val},
-    Result::Err(err) => {format!("OH NO")}    
+    Result::Err(err) => {format!("coinprice_id failed: {}",err)}    
     };
     let v: Value = serde_json::from_str(&cp).unwrap();
     println!("{}",v);
@@ -87,9 +93,8 @@ fn test_pull_id() {
 fn test_pull_count() {
     let cp = match coinprices(4) {
     Result::Ok(val) => {val},
-    Result::Err(err) => {format!("OH NO")}    
+    Result::Err(err) => {format!("coinprices failed: {}",err)}    
     };
     let v: Value = serde_json::from_str(&cp).unwrap();
-    //https://docs.serde.rs/serde_json/value/enum.Value.html
     assert!(v.as_array().unwrap().len() == 4);
 }
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ extern crate reqwest;
 extern crate serde_json;
 // JSON Parsing and Construction
 // https://github.com/serde-rs/json
-use serde_json::{Value, Error};
+use serde_json::{Value};
 
 mod getcointicker;
 
-use std::io;
+//use std::io;
 
 //https://lukaskalbertodt.github.io/term-painter/term_painter/
 use term_painter::ToStyle;
 use term_painter::Color::*;
-use term_painter::Attr::*;
+//use term_painter::Attr::*;
 use getcointicker::coinprices;
 
 fn main() {
@@ -23,12 +23,17 @@ fn main() {
     );
     let cp = match coinprices(4) {
         Result::Ok(val) => {val},
-        Result::Err(err) => {format!("OH NO")}
+        Result::Err(err) => {format!("Unable to get coin prices: {}",err)}
     };
     println!("{}",cp);  
     let v: Value = match serde_json::from_str(&cp){
         Result::Ok(val) => {val},
-        Result::Err(err) => {panic!("Unable to get item")}
+        Result::Err(err) => {panic!("Unable to parse json: {}",err)}
     };
+    
+    for coin in v.as_array() {
+        println!("{}",coin[0]);
+    }
+        
     println!("{}",v[0]["market_cap_usd"]);  
 }
